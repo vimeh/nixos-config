b2c7fc01ab7a1f8f6c0cfc1ccba5fde745962d1a
Vinay <vinaymehta.nyc@gmail.com>
2023-01-02 19:19:15 -0800

    nvim: add telescope, switch some mappings to which-key

--- home/nvim/lua/remap.lua
+++ home/nvim/lua/remap.lua
@@ -1,10 +1,15 @@
 local nnoremap = require("keymap").nnoremap
 local vnoremap = require("keymap").vnoremap
+local wk = require("which-key")
 
 -- Quality of life
-nnoremap("<leader>w", "<cmd>w<CR>")
-nnoremap("<leader>x", "<cmd>x<CR>")
-nnoremap("<leader>q", "<cmd>q<CR>")
+wk.register({
+	name = "QoL",
+	w = { "<cmd>w<cr>", "Save" },
+	q = { "<cmd>q<cr>", "Quit" },
+	x = { "<cmd>x<cr>", "Quit" },
+	e = { "<cmd>Neotree toggle<cr>", "Explorer" },
+}, { prefix = "<leader>" })
 
 -- Navigating splits
 nnoremap("<C-J>", "<C-W><C-J>")
@@ -16,10 +21,6 @@ nnoremap("<C-H>", "<C-W><C-H>")
 nnoremap("<leader>r", "<cmd>!python3 %<CR>")
 nnoremap("<leader>d", "<cmd>bd!<CR>")
 
--- Neotree
-nnoremap("<leader>e", "<cmd>Neotree toggle<CR>")
-nnoremap("<leader>o", "<cmd>Neotree focus<CR>")
-
 -- Rust Development
 -- nnoremap("<leader>s", "<cmd>%! rustfmt<CR>")
 
@@ -30,27 +31,29 @@ nnoremap("<leader>,", "<cmd>noh<CR>")
 vnoremap("<leader>c", '"+y<CR>')
 
 -- Telescope
-nnoremap("<leader>f", "<cmd>Telescope<CR>")
+local telescope = require("telescope.builtin")
+wk.register({
+	f = {
+		name = "Telescope",
+		f = { telescope.find_files, "Find Files" },
+		r = { telescope.oldfiles, "Recent Files" },
+		g = { telescope.git_files, "Find in Git Project" },
+		d = { telescope.live_grep, "Live Grep" },
+		b = { telescope.buffers, "Buffers" },
+		h = { telescope.help_tags, "Help Tags" },
+	},
+}, { prefix = "<leader>" })
 
 -- Harpoon
-nnoremap("<leader>a", function()
-	require("harpoon.mark").add_file()
-end, silent)
-nnoremap("<leader>s", function()
-	require("harpoon.ui").toggle_quick_menu()
-end, silent)
-nnoremap("<leader>h", function()
-	require("harpoon.ui").nav_file(1)
-end, silent)
-nnoremap("<leader>j", function()
-	require("harpoon.ui").nav_file(2)
-end, silent)
-nnoremap("<leader>k", function()
-	require("harpoon.ui").nav_file(3)
-end, silent)
-nnoremap("<leader>l", function()
-	require("harpoon.ui").nav_file(4)
-end, silent)
+wk.register({
+	name = "Harpoon",
+	a = { require("harpoon.mark").add_file, "Add File" },
+	s = { require("harpoon.ui").toggle_quick_menu, "Toggle Quick Menu" },
+	h = { require("harpoon.ui").nav_file(1), "Navigate to File 1" },
+	j = { require("harpoon.ui").nav_file(2), "Navigate to File 2" },
+	k = { require("harpoon.ui").nav_file(3), "Navigate to File 3" },
+	l = { require("harpoon.ui").nav_file(4), "Navigate to File 4" },
+}, { prefix = "<leader>" })
 
 -- Terminal
 -- nnoremap("<leader>t", "<cmd>ToggleTerm<CR>")
