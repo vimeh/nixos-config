42798314ebe95c9904f20a4096870e0c8f5028b3
Vinay Mehta <vinaymehta.nyc@gmail.com>
2023-01-31 12:32:41 -0800

    nvim: splits and terminal key bindings

--- home/nvim/lua/remap.lua
+++ home/nvim/lua/remap.lua
@@ -9,16 +9,24 @@ wk.register({
   -- x = { "<cmd>x<cr>", "Quit" },
   e = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
   gg = { "<cmd>LazyGit<cr>", "LazyGit" },
-  t = { "<cmd>ToggleTerm<cr>", "Terminal" },
+  t = { "<cmd>ToggleTerm direction=vertical<cr>", "VTerminal" },
+  T = { "<cmd>ToggleTerm direction=horizontal<cr>", "HTerminal" },
   y = { '"+y', "SystemCopy", mode = "v" },
   p = { '"+p', "SystemPaste" },
+  n = { "<cmd>enew<cr>", "New editor" },
 }, { prefix = "<leader>" })
+
 -- Clear highlighting
 vim.keymap.set("n", "<leader>,", "<cmd>noh<CR>")
 
+
+wk.register({
+  vv = { "<cmd>vsplit<cr>", "VSplit" },
+})
 -- Windows
 local splits = require('smart-splits')
 -- moving between splits
+-- TODO: loop over modes
 vim.keymap.set('n', '<C-h>', splits.move_cursor_left)
 vim.keymap.set('n', '<C-j>', splits.move_cursor_down)
 vim.keymap.set('n', '<C-k>', splits.move_cursor_up)
@@ -31,12 +39,28 @@ vim.keymap.set('i', '<C-h>', splits.move_cursor_left)
 vim.keymap.set('i', '<C-j>', splits.move_cursor_down)
 vim.keymap.set('i', '<C-k>', splits.move_cursor_up)
 vim.keymap.set('i', '<C-l>', splits.move_cursor_right)
+-- and for terminal
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
 -- resizing splits
+-- TODO: add for other modes?
 vim.keymap.set('n', '<C-S-r>', splits.start_resize_mode)
 vim.keymap.set('n', '<C-S-h>', splits.resize_left)
 vim.keymap.set('n', '<C-S-j>', splits.resize_down)
 vim.keymap.set('n', '<C-S-k>', splits.resize_up)
 vim.keymap.set('n', '<C-S-l>', splits.resize_right)
+
 -- moving splits
 require("winshift").setup()
 vim.keymap.set('n', '<C-S-m>', [[<cmd>WinShift<cr>]])
@@ -56,12 +80,6 @@ wk.register({
     d = { telescope.live_grep, "Live Grep" },
     b = { telescope.buffers, "Buffers" },
     h = { telescope.help_tags, "Help Tags" },
-    n = {
-      name = "New file",
-      n = { "<cmd>enew<cr>", "New editor" },
-      s = { "<cmd>split<cr>", "New editor hsplit" },
-      S = { "<cmd>vsplit<cr>", "New editor vsplit" },
-    },
   },
 }, { prefix = "<leader>" })
 
@@ -108,19 +126,6 @@ wk.register({
     r = { vim.lsp.buf.references, "References" },
   },
 }, { prefix = "<leader>" })
-
-function _G.set_terminal_keymaps()
-  local opts = { buffer = 0 }
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-end
-
--- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 -- Persistence
 wk.register({
